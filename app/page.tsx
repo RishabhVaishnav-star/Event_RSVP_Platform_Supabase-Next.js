a84853f0d0219cbd6c1418b7585e5355ed05fbfa
RishabhVaishnav-star <rishabhvaishnav812@gmail.com>
2025-08-30 17:03:32 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react"
 import Link from "next/link"
 import { supabase } from "../lib/supabaseClient"
+import { DBEvent } from "./types";
 
 export default function HomePage() {
   const [events, setEvents] = useState<DBEvent[]>([])
